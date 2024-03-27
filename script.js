c98a24f9ac3d67cd39c6c2440e21ebcf9de2df47
Isa Coronel <156938989+icoronel1@users.noreply.github.com>
2024-03-27 16:15:11 +0000

    mouse pressed function

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ COMP 125 Lab 11 - Adding to an Array with mouse clicks
 Step 1 - add a line of code to function mousePressed() that will add a new object to the array when the mouse is pressed. The first part (creating a new object where the mouse is pressed) is done for you. Just add the line that appends it to the array.
 Step 2 - add comments explaining each line of code in this sketch.
 */
-var dots = new Array(1);
+var dots = new Array(1); //creating an array/list with 1 element and assign to variable 'dots'
 
 function setup() {
-  createCanvas(500, 300); 
-  dots[0] = new Dot(width/2, height/2);
+  createCanvas(500, 300); // canvas with width of 500 and height of 300 pixels
+  dots[0] = new Dot(width/2, height/2); 
 }
 
 function draw() {
@@ -26,6 +26,7 @@ function draw() {
 
 function mousePressed(){
   let obj = new Dot(mouseX, mouseY);
+  dots.push(obj);
   
   /* add a line of code that adds "obj" to the "dots" array. Use the .push() method of the dots array object to append the new "obj" to the end of an existing array. Check the Array.push() documentation first to make sure you are using it correctly.
   */
